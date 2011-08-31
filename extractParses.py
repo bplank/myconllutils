2db93e0525696f2371e8ebdcb48548175236db7c
bplank <bplank@gmail.com>
2011-08-31 10:25:53 +0200

    added extractParses (to get sentences of specific length) fixed import statements

--- extractParses.py
+++ extractParses.py
@@ -4,8 +4,8 @@
 Author: Barbara Plank
 Date:   August 2011
 
-Description: Given two CoNLL 2007 files, this script extracts the
-dependency parses the two systems agree upon.
+Description: This script extracts sentences from a CoNLL file
+of specific length
 
 Options: specify minimun and maximum sentence length to extract
 
@@ -15,7 +15,7 @@ from conll.Conll07Reader import Conll07Reader
 from optparse import OptionParser
 
 def main():
-    usage = "usage: %prog [options] FILE1 FILE2"
+    usage = "usage: %prog [options] FILE"
 
     parser = OptionParser(usage=usage)
     parser.add_option("--minLen", dest="minLen",default=0,type="int",
@@ -26,32 +26,22 @@ def main():
 
     (options, args) = parser.parse_args()
 
-    if len(args) < 2:
-        print("Arguments missing!")
+    if len(args) < 1:
+        print("Argument missing!")
         parser.print_help()
         exit(-1)
     
     file1 = args[0]
-    file2 = args[1]
 
     reader1 = Conll07Reader(file1)
-    reader2 = Conll07Reader(file2)
 
     instances1 = reader1.getInstances()
-    instances2 = reader2.getInstances()
     
     for i in instances1:
         if i.getSentenceLength() > options.minLen:
-            for j in instances2:
-                if not i.equalForm(j):
-                    continue
-                elif i.equalHeadsAndLabels(j):
-                    if options.maxLen:
-                        if i.getSentenceLength() <= options.maxLen:
-                            print(i)
-                    else:
-                        print(i)
-                    break
-                
- 
+            if options.maxLen and i.getSentenceLength() <= options.maxLen:
+                print(i)
+            else:
+                print(i)
+
 main()
